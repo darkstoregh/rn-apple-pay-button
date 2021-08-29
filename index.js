ffd910943183b81afe76ebcf6c6e0325be107bee
Inder Dhir <inderdhir@hotmail.com>
2021-08-29 12:44:48 -0400

    Remove unused import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { NativeModules, requireNativeComponent } from "react-native";
+import { requireNativeComponent } from "react-native";
 
 const APButtonComponent = requireNativeComponent("APButton");
 
